5c7a199b94999c2060dfd75ea506e8a0b9e3fdd5
Nicolas Corrarello <ncorrare@gmail.com>
2016-12-08 15:56:32 +0000

    Added full path for unless

--- manifests/init.pp
+++ manifests/init.pp
@@ -72,7 +72,7 @@ class windowstime (
   if $timezone {
     validate_re($timezone, $timezones, 'The specified string is not a valid Timezone')
     exec { "tzutil.exe /s \"$timezone\"":
-      unless => "cmd.exe /C tzutil.exe /g | findstr /C:\"$timezone\"",
+      unless => "C:/Windows/System32/cmd.exe /C tzutil.exe /g | findstr /C:\"$timezone\"",
       cwd    => 'C:/Windows/System32'
     }
   }
